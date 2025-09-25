c9bb61c038d2a7ee34610074d1b2c67587f77b30
marcio cosenza <marciocosenza.sw@gmail.com>
2025-09-25 15:22:17 -0300

    Update untMC2000T2.pas

--- Couth MC2000T/untMC2000T2.pas
+++ Couth MC2000T/untMC2000T2.pas
@@ -1,78 +1,99 @@
 unit untMC2000T2;
 
+
 interface
 
-uses Classes, SysUtils;
+uses Classes, SysUtils, Vcl.Dialogs, CPDrv, vcl.Forms, System.UITypes;
+
+
+{
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, CPDrv, Vcl.StdCtrls,
+  untMC2000T2, Vcl.Tabs, Vcl.ComCtrls, Vcl.ExtCtrls;
+ }
 
 Type
   TMC2000T2 = class
 
   public
 
+    constructor create(form: TForm);
+
     //controle da marcadora
-    function Avancar: string;
-    function Cancelar: string;
-    function Pausar: string;
-    function getStatus(): string;
+    function Avancar()  : string;
+    function Cancelar() : string;
+    function Pausar()   : string;
+    function Reset()   : string;
 
-    procedure setLinha(param: integer);
+    //Define qual Ã© a linha que serÃ¡ configurada na marcadora
+    procedure addLinha();
+    procedure iniciarLista();
 
-    //formatação da marcação
-    procedure setDados    (param: integer); // Os dados que serão marcados
-    procedure setAltChar  (param: integer); // Altura do caractere
-    procedure setLargChar (param: integer); // Largura do caractere
-    procedure setDens     (param: integer); // Densidade
-    procedure setPosX     (param: integer); // Posição X
-    procedure setPosY     (param: integer); // Posição Y
-    procedure setAngulo   (param: integer); // Angulo da marcação
-    procedure setSpeed    (param: integer); // Velocidade da marcação
-    procedure setForca    (param: integer); // Força da marcação
+    //formataÃ§Ã£o da marcaÃ§Ã£o
+    procedure setDados    (valor: string); // Os dados que serÃ£o marcados
+    procedure setAltChar  (valor: integer); // Altura do caractere
+    procedure setLargChar (valor: integer); // Largura do caractere
+    procedure setDens     (valor: integer); // Densidade
+    procedure setPosX     (valor: integer); // PosiÃ§Ã£o X
+    procedure setPosY     (valor: integer); // PosiÃ§Ã£o Y
+    procedure setAngulo   (valor: integer); // Angulo da marcaÃ§Ã£o
+    procedure setSpeed    (valor: integer); // Velocidade da marcaÃ§Ã£o
+    procedure setForca    (valor: integer); // ForÃ§a da marcaÃ§Ã£o
+
+
+    function getItemFuncao(qual: integer): string;  // Descritivo
+    function getItemMin   (qual: integer): integer; // Valor minimo aceito pelo equipamento
+    function getItemMax   (qual: integer): integer; // Valor maximo aceito pelo equipamento
 
     function enviar(): boolean;
+    function getStatus(): string;
+
+
+    function conectar(): boolean;
+    function desConectar(): boolean;
+
 
 //    procedure setDatamatrix(dmc : string);
 //    procedure setLogo(arq: string);
 
   private
 
-    //quantidade de bytes por campo
-    const vcAltura = 2;
-    const vcLarg   = 1;
-    const vcPosX   = 2;
-    const vcPosY   = 2;
-    const vcDens   = 1;
-    const vcForca  = 1;
-    const vcSpeed  = 1;
-    const vcAngulo = 2;
+  const totalFuncoes = 8;
+
+    //estrutura destinada a armazenar as informaÃ§Ãµes dos campos
+    type itemFuncao = record
+        funcao : string;  // Descritivo
+        min    : integer; // Valor maximo aceito pelo equipamento
+        max    : integer; // Valor maximo aceito pelo equipamento
+        size   : integer; // Quantidade de bytes por campo
+        val    : string;  // valor que serÃ¡ marcado
+        command: string;  // string com a instrucao que serÃ¡ enviada a marcadora
+        status : boolean; // validaÃ§Ã£o
+    end;
 
     var
+      item         : itemFuncao;
+      listFuncaoes : array[0..totalFuncoes] of itemFuncao;
       linha        : integer;
       destinatario : string;
+      comPort      : TCommPortDriver;
 
-      vDados   :string;
 
-      vAltChar : string; // Altura do caractere
-      vLargChar: string; // Largura do caractere
-      vDens    : string; // Densidade
-      vPosX    : string; // Posição X
-      vPosY    : string; // Posição Y
-      vAngulo  : string; // Angulo da marcação
-      vSpeed   : string; // Velocidade da marcação
-      vForca   : string; // Força da marcação
 
-    function prepararFicheiro: string;
+
     procedure setDestinatario(destino: string);
+    function prepararFicheiro: string;
+    function send(linha: string): boolean;
 
+    function getDestinatario() : string;
+    function getFimFicheiro()  : string;
+
+    function converter(valorIn, tam: integer): string;
+    function validar(qual, valor: integer): boolean;
     function CRC(data: string): string;
 
-    function getDestinatario : string;
-    function getFimFicheiro  : string;
-
-    function getInicioLinha  : string;
-    function getFimLinha     : string;
-    function converter(valorIn, tam: integer): string;
-    procedure iniciarVariaveis();
-
+    function getInicioLinha()  : string;
+    function getFimLinha()     : string;
 
 end;
 
@@ -83,95 +104,104 @@ var
 implementation
 
 
+function TMC2000T2.send(linha: string): boolean;
+begin
+  Result := False;
+
+//  if (comPort.Connected) then
+//    if (comPort.SendString(linha)) then
+      Result := true;
+end;
+
 { TTMC2000T2 }
 
 
 // ==================================
-// ===== DEFINIÇÕES DA MARCAÇÃO =====
+// ===== DEFINIÃ‡Ã•ES DA MARCAÃ‡ÃƒO =====
 // ==================================
 
-procedure TMC2000T2.setAltChar(param: integer);
-var
-  val: string;
+procedure TMC2000T2.setAltChar(valor: integer);
+const pos = 0;
 begin
-  val  := converter(param, vcAltura);
-  vAltChar := getInicioLinha() + chr(11) + chr(0) + chr(38) + chr(linha) + chr(1) +  val  + getFimLinha();
+  if (validar(pos, valor)) then
+      listFuncaoes[pos].command := getInicioLinha() + chr(11) + chr(0) + chr(38) + chr(linha) + chr(1) +  listFuncaoes[pos].val  + getFimLinha();
 end;
 
 // ==================================
-procedure TMC2000T2.setAngulo(param: integer);
-var
-  val: string;
+procedure TMC2000T2.setLargChar(valor: integer);
+const pos = 1;
 begin
-  val  := converter(param, vcAngulo);
-  vAngulo := getInicioLinha() + chr(11) + chr(0) + chr(38) + chr(linha) + chr(9) + val + getFimLinha();
+  if (validar(pos, valor)) then
+    listFuncaoes[pos].command := getInicioLinha() + chr(10) + chr(0) + chr(38) + chr(linha) + chr(2) + listFuncaoes[pos].val + getFimLinha();
+end;
+
+
+// ==================================
+procedure TMC2000T2.setDens(valor: integer);
+const pos = 2;
+begin
+  if (validar(pos, valor)) then
+      listFuncaoes[pos].command := getInicioLinha() + chr(10) + chr(0) + chr(38) + chr(linha) + chr(5) + listFuncaoes[pos].val + getFimLinha();
 end;
 
 // ==================================
-procedure TMC2000T2.setDados(param: integer);
+procedure TMC2000T2.setPosX(valor: integer);
+const pos = 3;
+begin
+    if (validar(pos, valor)) then
+      listFuncaoes[pos].command := getInicioLinha() + chr(11) + chr(0) + chr(38) + chr(linha) + chr(6) + listFuncaoes[pos].val + getFimLinha();
+end;
+
+// ==================================
+procedure TMC2000T2.setPosY(valor: integer);
+const pos = 4;
+begin
+  if (validar(pos, valor)) then
+    listFuncaoes[pos].command := getInicioLinha() + chr(11) + chr(0) + chr(38) + chr(linha) + chr(7) + listFuncaoes[pos].val + getFimLinha();
+end;
+
+// ==================================
+procedure TMC2000T2.setAngulo(valor: integer);
+const pos = 5;
+begin
+  if (validar(pos, valor)) then
+      listFuncaoes[pos].command := getInicioLinha() + chr(11) + chr(0) + chr(38) + chr(linha) + chr(9) + listFuncaoes[pos].val + getFimLinha();
+end;
+
+// ==================================
+procedure TMC2000T2.setSpeed(valor: integer);
+const pos = 6;
+begin
+  if (validar(pos, valor)) then
+    listFuncaoes[pos].command := getInicioLinha() + chr(11) + chr(0) + chr(38) + chr(linha) + chr(14) + listFuncaoes[pos].val + getFimLinha();
+end;
+
+// ==================================
+procedure TMC2000T2.setForca(valor: integer);
+const pos = 7;
+begin
+  if (validar(pos, valor)) then
+      listFuncaoes[pos].command := getInicioLinha() + chr(11) + chr(0) + chr(38) + chr(linha) + chr(15) + listFuncaoes[pos].val + getFimLinha();
+end;
+
+
+
+// ==================================
+procedure TMC2000T2.setDados(valor: string);
+const pos = 8;
+
 var
-  val: string;
+//  val: string;
   tam: integer;
+
 begin
 //  val  := converter(param, vc);
-//  tam    := Length(val) + 8;
-
-//  Result := getInicioLinha() + chr(tam) + chr(0) + chr(37) + chr(linha) + val + getFimLinha();
+  tam    := Length(valor) + 8;
+//  if (validar(pos, valor)) then
+      listFuncaoes[pos].command := getInicioLinha() + chr(tam) + chr(0) + chr(37) + chr(linha) + valor + getFimLinha();
+///  Result := getInicioLinha() + chr(tam) + chr(0) + chr(37) + chr(linha) + val + getFimLinha();
 end;
 
-// ==================================
-procedure TMC2000T2.setDens(param: integer);
-var
-  val: string;
-begin
-  val  := converter(param, vcDens);
-  vDens := getInicioLinha() + chr(10) + chr(0) + chr(38) + chr(linha) + chr(5) + val + getFimLinha();
-end;
-
-// ==================================
-procedure TMC2000T2.setForca(param: integer);
-var
-  val: string;
-begin
-  val  := converter(param, vcForca);
-  vForca := getInicioLinha() + chr(11) + chr(0) + chr(38) + chr(linha) + chr(15) + val + getFimLinha();
-end;
-
-// ==================================
-procedure TMC2000T2.setLargChar(param: integer);
-var
-  val: string;
-begin
-  val  := converter(param, vcLarg);
-  vLargChar := getInicioLinha() + chr(10) + chr(0) + chr(38) + chr(linha) + chr(2) + val + getFimLinha();
-end;
-
-// ==================================
-procedure TMC2000T2.setPosX(param: integer);
-var
-  val: string;
-begin
-  val  := converter(param, vcPosX);
-  vPosX := getInicioLinha() + chr(11) + chr(0) + chr(38) + chr(linha) + chr(6) + val + getFimLinha();
-end;
-
-// ==================================
-procedure TMC2000T2.setPosY(param: integer);
-var
-  val: string;
-begin
-  val  := converter(param, vcPosY);
-  vPosY := getInicioLinha() + chr(11) + chr(0) + chr(38) + chr(linha) + chr(7) + val + getFimLinha();
-end;
-
-// ==================================
-procedure TMC2000T2.setSpeed(param: integer);
-var
-  val: string;
-begin
-  val  := converter(param, vcSpeed);
-  vSpeed := getInicioLinha() + chr(11) + chr(0) + chr(38) + chr(linha) + chr(14) + val + getFimLinha();
-end;
 
 
 
@@ -185,7 +215,8 @@ var
   aux : string;
 begin
   aux := chr(16) + getDestinatario + chr(7) + chr(0) + chr(49);//   '070031';
-  Result := aux+CRC(aux);
+  aux := aux + CRC(aux);
+  send(aux);
 end;
 
 //=================================
@@ -194,7 +225,8 @@ var
   aux : string;
 begin
   aux := chr(16) + getDestinatario + chr(7) + chr(0) + chr(52);//   '070031';
-  Result := aux + CRC(aux);
+  aux := aux + CRC(aux);
+  send(aux);
 end;
 
 //=================================
@@ -203,12 +235,14 @@ var
   aux : string;
 begin
   aux := chr(16) + getDestinatario + chr(7) + chr(0) + chr(51);//   '070031';
-  Result := aux+CRC(aux);
+  aux := aux + CRC(aux);
+  send(aux);
 end;
 
 
+
 // =================================
-// ==== FORMATAÇÃO DA MARCAÇÃO =====
+// ==== FORMATAÃ‡ÃƒO DA MARCAÃ‡ÃƒO =====
 // =================================
 
 
@@ -247,17 +281,93 @@ begin
   Result := aux+CRC(aux);
 end;
 
-procedure TMC2000T2.iniciarVariaveis;
+procedure TMC2000T2.iniciarLista();
+var
+  I: Integer;
+
 begin
-  vDados    := '';
-  vAltChar  := '';
-  vLargChar := '';
-  vDens     := '';
-  vPosX     := '';
-  vPosY     := '';
-  vAngulo   := '';
-  vSpeed    := '';
-  vForca    := '';
+
+
+linha := 0;
+
+for I := 0 to totalFuncoes do
+  begin
+
+    case I of
+
+      0:begin
+          item.funcao := 'Altura';
+          item.min    := 0;
+          item.max    := 400;
+          item.size   := 2;
+        end;
+
+
+      1:begin
+          item.funcao := 'Largura';
+          item.min    := 0;
+          item.max    := 200;
+          item.size   := 1;
+        end;
+
+      2:begin
+          item.funcao := 'Densidade';
+          item.min    := 1;
+          item.max    := 101;
+          item.size   := 1;
+        end;
+
+      3:begin
+          item.funcao := 'Pos X';
+          item.min    := 0;
+          item.max    := 10000;
+          item.size   := 2;
+        end;
+
+      4:begin
+          item.funcao := 'Pos Y';
+          item.min    := 0;
+          item.max    := 10000;
+          item.size   := 2;
+        end;
+
+      5:begin
+          item.funcao := 'Angulo';
+          item.min    := 0;
+          item.max    := 3600;
+          item.size   := 2;
+        end;
+
+      6:begin
+          item.funcao := 'Velocidade';
+          item.min    := 1;
+          item.max    := 10;
+          item.size   := 1;
+        end;
+
+      7:begin
+          item.funcao := 'ForÃ§a';
+          item.min    := 1;
+          item.max    := 10;
+          item.size   := 1;
+        end;
+       {
+      8:begin
+          item.funcao := 'EspaÃ§amento';
+          item.min    := 0;
+          item.max    := 200;
+          item.size   := 1;
+        end;
+        }
+
+    end;
+
+    item.status     := False;
+    item.command    := '';
+
+    listFuncaoes[I] := item;
+  end;
+
 end;
 
 {
@@ -272,10 +382,15 @@ end;
 
 Function TMC2000T2.prepararFicheiro: string;
 begin
-  iniciarVariaveis();
   Result := chr(16) +chr(2) + chr(7) + chr(0) + chr(33) + chr(3) +chr(3);
 end;
 
+
+function TMC2000T2.Reset: string;
+begin
+  send(prepararFicheiro);
+  iniciarLista();
+end;
 
 procedure TMC2000T2.setDestinatario(destino: string);
 begin
@@ -291,10 +406,77 @@ end;
 
 
 
+constructor TMC2000T2.create(form: TForm);
+begin
+  comPort := TCommPortDriver.Create(form);
+
+  comPort.PortName := 'COM1';
+  comPort.BaudRate := TBaudRate.br9600;// := 9600;
+  comPort.StopBits := TStopBits.sb1BITS;
+  comPort.SwFlow   := sfNONE;
+  comPort.DataBits := db8BITS;
+  comPort.Parity   := ptNONE;
+
+  iniciarLista();
+end;
+
+function TMC2000T2.desConectar: boolean;
+begin
+  comPort.Disconnect();
+end;
+
 ////////////////
+
+
+
+function TMC2000T2.validar(qual, valor: integer): boolean;
+var
+  min, max : integer;
+  funcao   : string;
+  saidaTxt : string;
+begin
+
+  Result := False;
+  min    := listFuncaoes[qual].min;
+  max    := listFuncaoes[qual].max;
+
+  if ((valor >= min)  and (valor <= max )) then
+    begin
+      Result := True;
+      listFuncaoes[qual].val := converter(valor, listFuncaoes[qual].size);
+    end
+  else
+    begin
+      funcao   :=  listFuncaoes[qual].funcao;
+      saidaTxt := 'O valor para ' + funcao + ' deve ser' + #13  + #13;
+      saidaTxt :=  saidaTxt +' Maior que: ' + min.ToString  + #13;
+      saidaTxt :=  saidaTxt +' Menor que: ' + max.ToString  + #13;
+      saidaTxt :=  saidaTxt +' valor informado:  ' + valor.ToString;
+      ShowMessage(saidaTxt);
+    end;
+
+    listFuncaoes[qual].status := Result;
+end;
+
+
 function TMC2000T2.getInicioLinha(): string;
 begin
  Result := chr(16) + chr(2);
+end;
+
+function TMC2000T2.getItemFuncao(qual: integer): string;
+begin
+  Result := listFuncaoes[qual].funcao;
+end;
+
+function TMC2000T2.getItemMax(qual: integer): integer;
+begin
+  Result := listFuncaoes[qual].max;
+end;
+
+function TMC2000T2.getItemMin(qual: integer): integer;
+begin
+  Result := listFuncaoes[qual].min;
 end;
 
 function TMC2000T2.getFimLinha(): string;
@@ -302,6 +484,16 @@ begin
   Result :=  chr(3) +chr(3);
 end;
 
+
+function TMC2000T2.conectar: boolean;
+begin
+  Result := true;
+  if (not (comPort.Connect())) then
+    begin
+      Result := false;
+      MessageDlg('Falha ao efetuar a conexao com a marcadora Couth MC2000!', mtInformation, [mbok], 0);
+    end;
+end;
 
 function TMC2000T2.converter(valorIn, tam: integer): string;
 var
@@ -327,19 +519,49 @@ begin
 end;
 
 
-procedure TMC2000T2.setLinha(param: integer);
+procedure TMC2000T2.addLinha();
 begin
-  linha := param;
+  linha := linha + 1;
 end;
 
 
 
 function TMC2000T2.enviar: boolean;
+var
+  I: Integer;
+  isOK: boolean;
 begin
-  prepararFicheiro();
 
-  iniciarVariaveis();
+  //verifica se todas as funÃ§oes foram carregadas corretamente
+  isOK := true;
+  for I := 0 to totalFuncoes do
+      if ((isOK) and (not (listFuncaoes[I].status))) then
+          isOK := false;
+
+
+
+  if (isOK) then
+    begin
+
+      //enviad o ficheiro,
+      isOK :=  send(prepararFicheiro());
+      if (isOK) then
+        //envia as linhas
+        for I := 0 to totalFuncoes do
+          if (isOK) then
+            isOK :=  send(listFuncaoes[I].command);
+
+      //reseta as variaveis
+      if (isOK) then iniciarLista();
+    end;
+
+ Result := isOK;
 end;
 
 
 end.
+
+
+
+
+
